2985df7626ada2cfe89ddd88c9af8d6bea7ab244
tahirmirji <tahiritguru@gmail.com>
2023-12-27 15:40:26 +0530

    alter

--- index.php
+++ index.php
@@ -17,17 +17,19 @@ else
     
     // $query="CREATE TABLE Employee_Details(Empid int(10) NOT NULL auto_increment PRIMARY KEY, empname varchar(30) NOT NULL)";
 
-    $q="INSERT INTO contact_book values ('Friend1',8789524870)";
+    // $q="INSERT INTO contact_book values ('Friend1',8789524870)";
+
+    $Alter_Query="ALTER TABLE contact_book ADD addresses varchar(100) NOT NULL AFTER Phone_Num";
     
-    $result=mysqli_query($connection,$q);
+    $result=mysqli_query($connection,$Alter_Query);
     
     if($result)
     {
-        echo "New Record Creation Successful ";
+        echo "New Column Creation Successful ";
     }
     else
     {
-        echo "New Record Creation Unsuccessful ";
+        echo "New Column Creation Unsuccessful ";
     }
 }
 ?>
